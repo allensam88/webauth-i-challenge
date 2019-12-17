53c530d0e3ed6b6b6f6d3591085fff157514789e
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-17 11:16:41 -0800

    added session persistence with connect-session-knex middleware

--- api/middleware-config.js
+++ api/middleware-config.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const session = require('express-session');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(sessions);
+const knex = require('../data/dbConfig.js');
 
 const sessionConfig = {
     name: 'monkey',
     secret: 'keep it secret, keep it safe!',
     saveUninitialized: false, // GDPR laws against setting cookies automatically
     resave: false,
+    // do not forget to add the 'new' keyword
+    store: new KnexSessionStore({
+        knex, // import from dbConfig.js
+        createtable: true,
+        // optional
+        clearInterval: 1000 * 60 * 10,
+        tablename: 'sessions',
+        sidfieldname: 'sid'
+    }),
     cookie: {
-        maxAge: 1000 * 30,
+        maxAge: 1000 * 60 * 10,
         secure: false, // dynamically change to true in production environment
         httpOnly: true
     }
@@ -19,5 +30,5 @@ module.exports = server => {
   server.use(helmet());
   server.use(express.json());
   server.use(cors());
-  server.use(session(sessionConfig));
+  server.use(sessions(sessionConfig));  // add a req.session object
 };
